471190ff4c011c97ce6d228461c7087381a6028e
aungzanbaw <aungzanbaw@gmail.com>
2015-12-26 22:06:33 +0630

    Get basic gems and setup workouts

--- Gemfile
+++ Gemfile
@@ -24,7 +24,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'haml'
+
 gem 'simple_form' 
+
 gem 'bootstrap-sass'
 
 group :development, :test do
---
+++ app/controllers/workouts_controller.rb
@@ -0,0 +1,16 @@
+class WorkoutsController < ApplicationController
+	def index
+		
+	end
+
+	def new		
+	end
+
+	def create		
+	end
+
+	private	
+		def method_name
+			
+		end
+end
